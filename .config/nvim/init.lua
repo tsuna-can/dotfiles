953a9d730f892318ad11353ea2bd7f4c7d170337
tsunacan <sin2016iba@gmail.com>
2024-06-30 08:11:44 +0900

    update brewfile

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,6 @@
+require('options')
+require('keymaps')
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   vim.fn.system({
@@ -24,8 +27,6 @@ require("lazy").setup("plugins", {
   },
 })
 
-require('options')
-require('keymaps')
 require('lualine').setup()
 require("toggleterm").setup{
   direction = 'float',
