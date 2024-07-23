dbe693189e78465fc35eb23ac83d25ab6b64119f
tsunacan <sin2016iba@gmail.com>
2024-07-23 20:28:08 +0900

    update shell script to create symlink

--- setup.sh
+++ setup.sh
@@ -1,20 +1,22 @@
-# ideavim
-ln -fs ${PWD}/.ideavimrc ~/.ideavimrc
+#!/bin/zsh
 
-# zsh
-ln -fs ${PWD}/.zshrc ~/.zshrc
+# Function to create a symbolic link if the file does not exist
+create_symlink() {
+  local target_file="$1"
+  local link_name="$2"
 
-# wezterm
-ln -fs ${PWD}/.wezterm.lua ~/.wezterm.lua
+  if [ -e "$link_name" ]; then
+    echo "File or directory $link_name already exists. Skipping..."
+  else
+    ln -fsv "${PWD}/${target_file}" "$link_name"
+  fi
+}
 
-# .config
-SOURCE_DIR="${PWD}/.config"
-TARGET_DIR="${HOME}/.config"
-
-for ITEM in "$SOURCE_DIR"/*; do
-  BASENAME=$(basename "$ITEM")
-  TARGET_ITEM="$TARGET_DIR/$BASENAME"
-
-  ln -fs $ITEM $TARGET_ITEM
-done
+create_symlink ".zshrc" "${HOME}/.zshrc"
+create_symlink ".wezterm.lua" "${HOME}/.wezterm.lua"
+create_symlink ".hammerspoon" "${HOME}/.hammerspoon"
+create_symlink ".ideavimrc" "${HOME}/.ideavimrc"
+create_symlink ".config/nvim" "${HOME}/.config/nvim"
+create_symlink ".config/sheldon" "${HOME}/.config/sheldon"
+create_symlink ".config/starship.toml" "${HOME}/.config/starship.toml"
 
