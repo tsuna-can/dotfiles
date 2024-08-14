c899a2b11c09871932e96f87f5254b6a87e3b8f4
tsunacan <sin2016iba@gmail.com>
2024-08-14 14:42:41 +0900

    edit buffer keymaps

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -21,26 +21,26 @@ map('n', '<C-p>', ':Telescope find_files find_command=rg,--files,--hidden,--glob
 map('n', '<C-g>', ':Telescope live_grep<CR>', { noremap = true })
 
 -- Move to previous/next buffer
-map('n', '<A-,>', '<Cmd>BufferPrevious<CR>', opts)
-map('n', '<A-.>', '<Cmd>BufferNext<CR>', opts)
--- Re-order to previous/next buffer
-map('n', '<A-<>', '<Cmd>BufferMovePrevious<CR>', opts)
-map('n', '<A->>', '<Cmd>BufferMoveNext<CR>', opts)
+map('n', '<leader>,', '<Cmd>BufferPrevious<CR>', opts)
+map('n', '<leader>.', '<Cmd>BufferNext<CR>', opts)
+-- -- Re-order to previous/next buffer
+-- map('n', '<leader><', '<Cmd>BufferMovePrevious<CR>', opts)
+-- map('n', '<leader>>', '<Cmd>BufferMoveNext<CR>', opts)
 -- Goto buffer in position...
-map('n', '<A-1>', '<Cmd>BufferGoto 1<CR>', opts)
-map('n', '<A-2>', '<Cmd>BufferGoto 2<CR>', opts)
-map('n', '<A-3>', '<Cmd>BufferGoto 3<CR>', opts)
-map('n', '<A-4>', '<Cmd>BufferGoto 4<CR>', opts)
-map('n', '<A-5>', '<Cmd>BufferGoto 5<CR>', opts)
-map('n', '<A-6>', '<Cmd>BufferGoto 6<CR>', opts)
-map('n', '<A-7>', '<Cmd>BufferGoto 7<CR>', opts)
-map('n', '<A-8>', '<Cmd>BufferGoto 8<CR>', opts)
-map('n', '<A-9>', '<Cmd>BufferGoto 9<CR>', opts)
-map('n', '<A-0>', '<Cmd>BufferLast<CR>', opts)
+map('n', '<leader>1', '<Cmd>BufferGoto 1<CR>', opts)
+map('n', '<leader>2', '<Cmd>BufferGoto 2<CR>', opts)
+map('n', '<leader>3', '<Cmd>BufferGoto 3<CR>', opts)
+map('n', '<leader>4', '<Cmd>BufferGoto 4<CR>', opts)
+map('n', '<leader>5', '<Cmd>BufferGoto 5<CR>', opts)
+map('n', '<leader>6', '<Cmd>BufferGoto 6<CR>', opts)
+map('n', '<leader>7', '<Cmd>BufferGoto 7<CR>', opts)
+map('n', '<leader>8', '<Cmd>BufferGoto 8<CR>', opts)
+map('n', '<leader>9', '<Cmd>BufferGoto 9<CR>', opts)
+map('n', '<leader>0', '<Cmd>BufferLast<CR>', opts)
 -- Pin/unpin buffer
-map('n', '<A-p>', '<Cmd>BufferPin<CR>', opts)
+map('n', '<leader>p', '<Cmd>BufferPin<CR>', opts)
 -- Close buffer
-map('n', '<A-c>', '<Cmd>BufferClose<CR>', opts)
+map('n', '<leader>c', '<Cmd>BufferClose<CR>', opts)
 
 -- Format
 map(
