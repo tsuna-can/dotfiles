029575faaf3c6ac4cec9e07b5c458d1f69668e99
tsunacan <sin2016iba@gmail.com>
2024-08-25 21:27:30 +0900

    change style of notification

--- .config/nvim/lua/plugins/noice-nvim.lua
+++ .config/nvim/lua/plugins/noice-nvim.lua
@@ -10,23 +10,37 @@ return {
     -- OPTIONAL:
     --   `nvim-notify` is only needed, if you want to use the notification view.
     --   If not available, we use `mini` as the fallback
-    "rcarriga/nvim-notify",
+    -- "rcarriga/nvim-notify",
   },
   config = function()
     require("noice").setup({
+      notify = {
+        enabled = true,
+        view = "mini",
+      },
       lsp = {
         override = {
           ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
           ["vim.lsp.util.stylize_markdown"] = true,
-          ["cmp.entry.get_documentation"] = true,   -- requires hrsh7th/nvim-cmp
+          ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
         },
       },
       presets = {
-        bottom_search = true,           -- use a classic bottom cmdline for search
-        command_palette = true,         -- position the cmdline and popupmenu together
-        long_message_to_split = true,   -- long messages will be sent to a split
-        inc_rename = false,             -- enables an input dialog for inc-rename.nvim
-        lsp_doc_border = false,         -- add a border to hover docs and signature help
+        bottom_search = true,         -- use a classic bottom cmdline for search
+        command_palette = true,       -- position the cmdline and popupmenu together
+        long_message_to_split = true, -- long messages will be sent to a split
+        inc_rename = false,           -- enables an input dialog for inc-rename.nvim
+        lsp_doc_border = false,       -- add a border to hover docs and signature help
+      },
+      routes = {
+        {
+          filter = {
+            event = "msg_show",
+            kind = "",
+            find = "written",
+          },
+          opts = { skip = true },
+        },
       },
     })
   end,
