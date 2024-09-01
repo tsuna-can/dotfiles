3fd1f964aed91e898c6ee6f103c00d73676da593
tsunacan <sin2016iba@gmail.com>
2024-09-01 18:43:00 +0900

    edit some setting

---
+++ .config/nvim/lua/plugins/cmp-cmdline.lua
@@ -0,0 +1,28 @@
+local M = { "hrsh7th/cmp-cmdline" }
+M.dependencies = { "nvim-cmp" }
+M.keys = { ":", "/", "?" }
+
+M.config = function()
+  local cmp = require("cmp")
+
+  -- normal Ex command
+  cmp.setup.cmdline(":", {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+      { name = "fuzzy_path" },
+    }, {
+      { name = "cmdline" },
+    }),
+  })
+
+  -- forward and backward search command
+  cmp.setup.cmdline({ "?", "/" }, {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+      { name = "treesitter" },
+    },
+  })
+end
+
+return M
+
