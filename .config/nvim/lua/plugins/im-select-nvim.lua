7e8e38034af5cba15d1d6e303cc7e59cfdbbfe4e
tsunacan <sin2016iba@gmail.com>
2025-03-21 21:28:55 +0900

    set default command of im-select.nvim

--- .config/nvim/lua/plugins/im-select-nvim.lua
+++ .config/nvim/lua/plugins/im-select-nvim.lua
@@ -1,6 +1,7 @@
 return {
   "keaising/im-select.nvim",
   opts = {
+    default_command     = "im-select",
     default_im_select   = "com.apple.keylayout.ABC",
     set_default_events  = { "VimEnter", "InsertEnter", "InsertLeave" },
     set_previous_events = {},
