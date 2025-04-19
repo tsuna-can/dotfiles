48bf5419addd055882d29d09090cd64d71ba5df2
tsunacan <sin2016iba@gmail.com>
2025-04-19 12:04:22 +0900

    delete without affecting registers

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -3,10 +3,11 @@ local opts = { noremap = true, silent = true }
 vim.g.mapleader = ' '
 
 -- Select all
-map("n", "<C-a>", "gg<S-v>G")
+map("n", "<leader>a", "gg<S-v>G")
 
 -- Delete without affecting the registers
 map("n", "x", '"_x', opts)
+map({ 'n', 'v' }, '<leader>d', '"_d', { noremap = true, silent = true })
 
 -- LSP
 map('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
@@ -20,10 +21,10 @@ map('n', '<C-p>', ':Telescope find_files find_command=rg,--files,--hidden,--glob
 map('n', '<C-g>', ':Telescope live_grep<CR>', { noremap = true })
 
 -- Move to previous/next buffer
-map('n', '<leader>,', '<Cmd>BufferPrevious<CR>', opts)
-map('n', '<leader>.', '<Cmd>BufferNext<CR>', opts)
--- Save and cloase all buffer
-map('n', '<leader>q', '<Cmd>wa | %bd<CR>', opts)
+map('n', '<leader>,', '<cmd>bprev<CR>', opts)
+map('n', '<leader>.', '<cmd>bnext<CR>', opts)
+-- Save and close all buffer
+map('n', '<leader>q', '<cmd>wa | %bd<CR>', opts)
 -- Move current buffer to the right pane
 map('n', '<leader>>', ':wincmd L<CR>', opts)
 -- Move current buffer to the left pane
@@ -40,9 +41,8 @@ map(
 map('n', '<leader>w', ':w<CR>', opts)
 
 -- DAP
-map('n', '<leader>du', '<cmd>lua require"dapui".toggle()<CR>', {noremap = true, desc = "Toggle DAP UI"})
-map('n', '<leader>dc', '<cmd>lua require"dap".continue()<CR>', {noremap = true, desc = "Continue Debugging"})
-map('n', '<leader>db', '<cmd>lua require"dap".toggle_breakpoint()<CR>', {noremap = true, desc = "Toggle Breakpoint"})
-map('n', '<leader>dt', '<cmd>lua require"dap".terminate()<CR>', {noremap = true, desc = "Terminate Debugging"})
-map('n', '<leader>dr', '<cmd>lua require"dap".repl.open()<CR>', {noremap = true, desc = "Open REPL"})
-
+map('n', '<leader>du', '<cmd>lua require"dapui".toggle()<CR>', { noremap = true, desc = "Toggle DAP UI" })
+map('n', '<leader>dc', '<cmd>lua require"dap".continue()<CR>', { noremap = true, desc = "Continue Debugging" })
+map('n', '<leader>db', '<cmd>lua require"dap".toggle_breakpoint()<CR>', { noremap = true, desc = "Toggle Breakpoint" })
+map('n', '<leader>dt', '<cmd>lua require"dap".terminate()<CR>', { noremap = true, desc = "Terminate Debugging" })
+map('n', '<leader>dr', '<cmd>lua require"dap".repl.open()<CR>', { noremap = true, desc = "Open REPL" })
