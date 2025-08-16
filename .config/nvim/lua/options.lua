e6900db9646c45be641718212c612f305f27ca6f
tsunacan <sin2016iba@gmail.com>
2025-08-16 17:57:48 +0900

    removed Copilot

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -7,10 +7,9 @@ opt.hlsearch = true
 opt.scrolloff = 30
 opt.clipboard = "unnamedplus"
 opt.cursorline = true
-opt.laststatus = 2
 opt.expandtab = true
 opt.tabstop = 2
 opt.shiftwidth = 2
 opt.wrap = true
 opt.laststatus = 3
-
+opt.showmatch = true
