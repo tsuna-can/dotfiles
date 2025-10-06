33d011d81cf19dd0e1905b605d5c6c2a008776ff
tsunacan <sin2016iba@gmail.com>
2025-10-06 15:34:57 +0900

    removed nvim-dap-python

--- .config/nvim/lua/plugins/nvim-dap-python.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "mfussenegger/nvim-dap-python",
-  dependencies = { "mfussenegger/nvim-dap" },
-  config = function()
-    local dap_python = require("dap-python")
-    dap_python.setup("uv venv exec python -m debugpy")
-  end
-}
